077878d18b3a88121f8f81940cdf6895e52640f4
W0ggi <voron529403@mail.ru>
2023-03-21 19:47:33 +0300

    registration 1.0

---
+++ Form_Authorization.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyWoggi
+{
+    public partial class Authorization : Form
+    {
+        public Authorization()
+        {
+            InitializeComponent();
+        }
+
+        private void Authorization_Load(object sender, EventArgs e)
+        {
+            Authorization_Panel.BorderStyle = BorderStyle.FixedSingle;
+        }
+    }
+}
